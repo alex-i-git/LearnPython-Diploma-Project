25ab63d69669e1fb4b47691110dd151955fd6820
Evgenia Koro <koro.eve@gmail.com>
2016-11-26 18:09:33 +0300

    название User где-то поменяли

--- site_with_map/server1.py
+++ site_with_map/server1.py
@@ -1,10 +1,20 @@
+from db import Survey
 from flask import Flask, render_template
 
 my_flask_app = Flask(__name__)
 
 @my_flask_app.route("/")
 def index():
-		return render_template("index.html")
+	surv = Survey
+	result = surv.query.filter(Survey.latitude > 0).filter(Survey.longitude > 0).all()
+	for item in result:
+		print(item)
+	return render_template("index.html", surveys=result)
+
+@my_flask_app.route("/about/")
+def about():
+
+	return render_template("about.html")
 
 if __name__ == "__main__":
 	my_flask_app.run(debug=True)
